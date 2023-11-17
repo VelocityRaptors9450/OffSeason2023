d58712c59a5776f1686dd0f8a6bc9afbfaf81998
LegendsNeverDl3 <1042154@apps.nsd.org>
2023-11-17 14:51:02 -0800

    update

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -7,6 +7,8 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -16,7 +18,10 @@ public class ShooterSubsystem extends SubsystemBase {
   //robot's perspectice left and right
   private final CANSparkMax leftFrontSpinMotor = new CANSparkMax(Constants.shooterFrontSpinL, MotorType.kBrushless);
   private final CANSparkMax rightFrontSpinMotor = new CANSparkMax(Constants.shooterFrontSpinR, MotorType.kBrushless);
- 
+
+  
+  //private final ProfiledPIDController backSpin = new ProfiledPIDController(0, 0, 0, new TrapezoidProfile.Constraints(5, 10));
+  
   public ShooterSubsystem() {
     leftFrontSpinMotor.follow(rightFrontSpinMotor, true);
     backSpinMotor.setInverted(true);

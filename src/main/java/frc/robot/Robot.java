2948b21d763040fded64cf17dd07a8a0e647fddb
KCheck25 <kcheck25@icloud.com>
2023-12-01 17:08:10 -0800

    changed literally two numbers still broken tho

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -573,12 +573,12 @@ public class Robot extends TimedRobot {
 					// auto
 					subsystems.drivebaseSubsystem.getKinematics(), // SwerveDriveKinematics
 					new PIDConstants(
-							10, 0.0,
+							5, 0.0,
 							0.0), // PID constants to correct for translation error (used to create the X and
 					// Y
 					// PID controllers)
 					new PIDConstants(
-						  10, 0.0,
+						  3, 0.0,
 							0.0), // PID constants to correct for rotation error (used to create the rotation
 					// controller)
 					subsystems.drivebaseSubsystem
